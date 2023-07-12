a383074ee170feae2efa013cf9058460c273c9b5
Kebalepile <pimpabutterfly86@gmail.com>
2023-07-12 12:26:19 +0200

    moved utils to common folder.

---
+++ common/utils.js
@@ -0,0 +1,23 @@
+export function printPorgress(count, max) {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  const percent = formatPercent(count / max);
+  process.stdout.write(`${count}/${max} (${percent})`);
+}
+
+function formatPercent(n) {
+  return (n * 100).toFixed(2) + "%";
+}
+
+export function groupBy(objArray, key) {
+  const groups = {};
+  for (let obj of objArray) {
+    const val = obj[key];
+    
+    if (groups[val] == null) {
+      groups[val] = [];
+    }
+    groups[val].push(obj);
+  }
+  return groups;
+}
